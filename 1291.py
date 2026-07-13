4e9c6b9bde0f7db2525b90858fbf41cd69929dc0
Yussufkadir <ysyurme1@stu.vistula.edu.pl>
2026-07-13 10:46:02 +0200

    new daily solution.

---
+++ 1291.py
@@ -0,0 +1,14 @@
+from typing import List
+def sequentialDigits(self, low: int, high: int) -> List[int]:
+    result = []
+
+    for length in range(2, 10):  
+        for start in range(1, 10 - length + 1):
+            num = 0
+            for digit in range(start, start + length):
+                num = num * 10 + digit
+            
+            if low <= num <= high:
+                result.append(num)
+    
+    return sorted(result)     
