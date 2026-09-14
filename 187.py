a5fec4c67dc8f2a69f20f19f8de4d4270cf09d23
Yussufkadir <ysyurme1@stu.vistula.edu.pl>
2026-09-14 21:14:52 +0200

    new solution (sliding window.)

---
+++ 187.py
@@ -0,0 +1,12 @@
+def findRepeatedDnaSequences(self, s: str) -> List[str]:
+    seen = set()
+    repeated = set()
+
+    for i in range(len(s) - 9):
+        substr = s[i : i+10]
+        if substr in seen:
+            repeated.add(substr)
+        else:
+            seen.add(substr)
+        
+    return list(repeated) 
